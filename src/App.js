432955dcc3d1985c5b19fb92addfa66b00d18905
TeeGee-TechBro <allin1acct.com@gmail.co>
2024-01-03 17:50:17 +0100

    more rtk crud query

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import {Data} from './components/Data'
-import {store} from './store'
-import {Provider} from 'react-redux'
+// import {store} from './store'
+// import {Provider} from 'react-redux'
 import {ApiProvider} from '@reduxjs/toolkit/query/react'
-import { productsApi } from './features/apiSlice';
+import { apiSlice } from './features/apiSlice';
 
 function App() {
   return (
     // <Provider store={store}>
-    <ApiProvider api={productsApi}>
+    <ApiProvider api={apiSlice}>
       <div className="App">
         <Data />
       </div>
