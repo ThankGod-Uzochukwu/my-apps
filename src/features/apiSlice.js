15cc9f657ccd2bbf2844f9948bdd38e92f5c6cbe
TeeGee Of Africa <114770639+ThankGod-Uzochukwu@users.noreply.github.com>
2025-09-22 04:17:39 +0100

    Revert "more rtk crud query"

--- src/features/apiSlice.js
+++ src/features/apiSlice.js
@@ -1,51 +1,17 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
-export const apiSlice = createApi({
+export const productsApi = createApi({
     reducerPath: 'productsApi',
-    // baseQuery: fetchBaseQuery({baseUrl: 'https://dummyjson.com'}),
-    baseQuery: fetchBaseQuery({baseUrl: 'https://jsonplaceholder.typicode.com'}),
-    tagTypes: ['Todos'],
+    baseQuery: fetchBaseQuery({baseUrl: 'https://dummyjson.com/'}),
     endpoints: (builder) => ({
         getAllProducts: builder.query({
-            query: () => '/todos',
-            transformResponse: res => res.sort((a, b) => b.id - a.id),
-            providesTags: ['Todos']
+            query: () => 'products',
         }),
-        // getProduct: builder.query({
-        //     query: (product) => `products/search?q=${product}`,
-        // })
-        addTodo: builder.mutation({
-            query: (todo) => ({
-                url: '/posts',
-                method: 'POST',
-                body: todo
-            }),
-            invalidateTags: ['Todos']
-        }),
-        updateTodo: builder.mutation({
-            query: (todo) => ({
-                url: `/posts/1${todo.id}`,
-                method: 'PATCH',
-                body: todo
-            }),
-            invalidateTags: ['Todos']
-        }),
-        deleteTodo: builder.mutation({
-            query: ({id}) => ({
-                url: `/posts/1${id}`,
-                method: 'DELETE',
-                body: id
-            }),
-            invalidateTags: ['Todos']
+        getProduct: builder.query({
+            query: (product) => `products/search?q=${product}`,
         })
     }),
 });
 
 
-export const {
-    useGetAllProductsQuery, 
-    useGetProductQuery, 
-    useAddTodoMutation, 
-    useUpdateTodoMutation, 
-    useDeleteTodoMutation
-} = apiSlice
+export const {useGetAllProductsQuery, useGetProductQuery} = productsApi
